ef1413f2cfd409b8a77dea231eb1b143a66a9f2d
anggarateo <114375533+anggarateo@users.noreply.github.com>
2024-10-28 06:51:15 +0700

    Update ChakraTheme.tsx: butler

--- app/ChakraTheme.tsx
+++ app/ChakraTheme.tsx
@@ -1,6 +1,6 @@
 import { extendTheme } from "@chakra-ui/react"
 import "@fontsource/newsreader"
-import "typeface-butler"
+import "@fontsource/butler/latin.css"
 
 const ChakraTheme = extendTheme({
   fonts: {
