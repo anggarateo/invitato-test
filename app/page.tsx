2452c86d7062381f0e44674073c3d2d6135bf5db
anggarateo50 <anggarateo50@gmail.com>
2024-10-27 22:26:01 +0700

    fix: minor

--- app/page.tsx
+++ app/page.tsx
@@ -95,6 +95,7 @@ export default function Home() {
                 Your browser does not support the audio element.
               </audio>
 
+              {isOpen &&
               <SlideFade
                 in={isOpen}
                 transition={{ enter: { duration: 1 } }}
@@ -118,6 +119,7 @@ export default function Home() {
                   <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" color="#FFFFFF" aria-hidden="true" focusable="false" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ color: 'rgb(255, 255, 255)' }}><path fill="none" d="M0 0h24v24H0z"></path><path d="M4.27 3 3 4.27l9 9v.28c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4v-1.73L19.73 21 21 19.73 4.27 3zM14 7h4V3h-6v5.18l2 2z"></path></svg>
                 </Button>
               </SlideFade>
+              }
             </Box>
 
             {isOpen &&
